1499064af63e567727223fe501cf368a9e403493
= <bensaidnadir@gmail.com>
2019-06-08 16:54:02 +0000

    full screen

--- desktop/src/org/java/ecluse/desktop/DesktopLauncher.java
+++ desktop/src/org/java/ecluse/desktop/DesktopLauncher.java
@@ -9,14 +9,14 @@ import org.java.ecluse.Ecluse;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		// DisplayMode primaryMode = config.getDesktopDisplayMode();
-		// config.title = "Ecluse simulation";
-		// config.setFromDisplayMode(primaryMode);
-		// config.vSyncEnabled = true;
+		DisplayMode primaryMode = config.getDesktopDisplayMode();
+		config.title = "Ecluse simulation";
+		config.setFromDisplayMode(primaryMode);
+		config.vSyncEnabled = true;
 		config.initialBackgroundColor = Color.NAVY;
-		config.resizable = false;
-		config.height = 1080;
-		config.width = 2048;
+		// config.resizable = false;
+		// config.height = 1080;
+		// config.width = 2048;
 		new LwjglApplication(new Ecluse(), config);
 	}
 }
